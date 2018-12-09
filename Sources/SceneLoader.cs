9923d87a27b710b5403e1d91f77eaa9943a64d5a
MorelAntoine <antoinemorel@outlook.com>
2018-12-09 18:23:38 -0500

    Add Scene Loader

---
+++ Sources/SceneLoader.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UniCraft.MenuSystem
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Module to load scene
+    /// </summary>
+    [AddComponentMenu("UniCraft/MenuSystem/SceneLoader")]
+    [DisallowMultipleComponent]
+    public sealed class SceneLoader : MonoBehaviour
+    {
+        ///////////////////////////////
+        ////////// Attribute //////////
+        ///////////////////////////////
+
+        ///////////////////////////////
+        ////////// Component //////////
+        
+        [SerializeField] private Slider _progressBar = null;
+        
+        /////////////////////////////////
+        ////////// Unity Event //////////
+
+        [SerializeField] private UnityEvent _onStartLoadingEvents = null;
+        
+        ////////////////////////////
+        ////////// Method //////////
+        ////////////////////////////
+        
+        /////////////////////////
+        ////////// API //////////
+        
+        public void LoadSceneByIndex(int sceneIndex)
+        {
+            StartCoroutine(LoadScene(SceneManager.LoadSceneAsync(sceneIndex)));
+        }
+        
+        public void LoadSceneByName(string sceneName)
+        {
+            StartCoroutine(LoadScene(SceneManager.LoadSceneAsync(sceneName)));
+        }
+        
+        /////////////////////////////
+        ////////// Service //////////
+
+        private IEnumerator LoadScene(AsyncOperation asyncOperation)
+        {
+            _onStartLoadingEvents.Invoke();
+            while ( !asyncOperation.isDone )
+            {
+                if ( _progressBar )
+                {
+                    _progressBar.value = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+                }
+                yield return null;
+            }
+        }
+    }
+}
