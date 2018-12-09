1e63ad93d48ccc5714fe2852f4b7695044faf998
MorelAntoine <antoinemorel@outlook.com>
2018-12-09 18:23:02 -0500

    Add PauseMenuSystem

---
+++ Sources/PauseMenuSystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace UniCraft.MenuSystem
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Module to manage pause menu
+    /// </summary>
+    [AddComponentMenu("UniCraft/MenuSystem/PauseMenu")]
+    [DisallowMultipleComponent]
+    public sealed class PauseMenuSystem : MonoBehaviour
+    {
+        ///////////////////////////////
+        ////////// Attribute //////////
+        ///////////////////////////////
+
+        [SerializeField] private UnityEvent _onOpenMenuEvents = null;
+        [SerializeField] private UnityEvent _onCloseMenuEvents = null;
+        [SerializeField] private UnityEvent _onOpenSettingsPanelEvents = null;
+        [SerializeField] private UnityEvent _onQuitGameEvents = null;
+        
+        ////////////////////////////
+        ////////// Method //////////
+        ////////////////////////////
+        
+        public void OpenMenu()
+        {
+            Time.timeScale = 0f;
+            _onOpenMenuEvents.Invoke();
+        }
+        
+        public void CloseMenu()
+        {
+            Time.timeScale = 1f;
+            _onCloseMenuEvents.Invoke();
+        }
+        
+        public void OpenSettingsPanel()
+        {
+            _onOpenSettingsPanelEvents.Invoke();
+        }
+        
+        public void QuitGame()
+        {
+            Time.timeScale = 1f;
+            _onQuitGameEvents.Invoke();
+        }        
+    }
+}
